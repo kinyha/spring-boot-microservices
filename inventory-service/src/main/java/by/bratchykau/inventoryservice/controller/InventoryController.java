b5ce74e0cd7eb4305b38e5c0b6b8d1ddc32d6f59
Bratchykau Uladzislau <kinyhaha@gmail.com>
2023-07-25 20:02:10 +0300

    dev : add connection inventory-serv with order service

--- inventory-service/src/main/java/by/bratchykau/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/by/bratchykau/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package by.bratchykau.inventoryservice.controller;
 
 
+import by.bratchykau.inventoryservice.dto.InventoryResponse;
 import by.bratchykau.inventoryservice.service.InventoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -16,10 +19,10 @@ public class InventoryController {
     private final InventoryService inventoryService;
 
 
-    @GetMapping("/{sku-code}")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
-        return inventoryService.isInStock(skuCode);
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCodes) {
+        return inventoryService.isInStock(skuCodes);
 
     }
 
--- order-service/src/main/java/by/bratchykau/orderservice/service/OrderService.java
+++ order-service/src/main/java/by/bratchykau/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package by.bratchykau.orderservice.service;
 
+import by.bratchykau.orderservice.dto.InventoryResponse;
 import by.bratchykau.orderservice.dto.OrderLineItemsDto;
 import by.bratchykau.orderservice.dto.OrderRequest;
 import by.bratchykau.orderservice.model.Order;
@@ -7,7 +8,9 @@ import by.bratchykau.orderservice.model.OrderLineItems;
 import by.bratchykau.orderservice.repostitory.OrderRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,10 +19,11 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, WebClient webClient) {
         this.orderRepository = orderRepository;
-
+        this.webClient = webClient;
     }
 
     public void placeOrder(OrderRequest orderRequest) {
@@ -33,7 +37,27 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        //call inventory service to check if we have enough inventory
+        InventoryResponse[] inventoryResponsArray = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCodes", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductionInStock = Arrays.stream(inventoryResponsArray)
+                .allMatch(InventoryResponse::isInStock);
+
+        if (allProductionInStock) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalStateException("Product inventory is not stock, please try again later");
+        }
+
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
