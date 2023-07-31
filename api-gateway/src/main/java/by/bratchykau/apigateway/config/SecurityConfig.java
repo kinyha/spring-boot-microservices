832e047a949bbe40d428da28faa8bed624bfae79
Bratchykau Uladzislau <kinyhaha@gmail.com>
2023-07-31 22:40:56 +0300

    dev : add security with keycloak

---
+++ api-gateway/src/main/java/by/bratchykau/apigateway/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package by.bratchykau.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+//@Configuration
+//public class SecurityConfig {
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.authorizeRequests((auth) -> auth
+//                .dispatcherTypeMatchers(HttpMethod.valueOf("/eureka/**")).permitAll()
+//                .anyRequest()
+//                .authenticated()
+//        );
+//
+//        return httpSecurity.build();
+//    }
+//}
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity
+                .csrf().disable()
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers("/eureka/**")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+        return serverHttpSecurity.build();
+    }
+}
